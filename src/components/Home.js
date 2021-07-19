447a8df823f936a06e8cfa23e03bc9a4031af7c4
eric santos <eric355402@yahoo.com>
2021-07-19 13:08:14 -0600

    added more text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import '../styles/home.css'
-import zombies from '../bkg-videos/zombies.webm'
+import React from "react";
+import "../styles/home.css";
+import zombies from "../bkg-videos/zombies.webm";
 
 const Home = () => {
-    return (
-        <div className="home">
+  return (
+    <div className="home">
       <video autoPlay muted loop id="myVideo">
         <source src={zombies} type="video/mp4" />
       </video>
       <header className="viewport-header">
         <h1>Ep Zombie Fest</h1>
+        <h2>Coming Soon</h2>
       </header>
     </div>
-    )
-}
-export default Home
+  );
+};
+export default Home;
